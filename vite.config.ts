0ea90bb6f785d01f26495512aa5996b94f0580cb
YiHsuan85 <yihsuanwu85@gmail.com>
2026-04-09 13:48:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Dietitian-workshop/', // 這裡要對應您的 GitHub 倉庫名稱
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // 重要：這裡必須對應您的 GitHub 倉庫名稱，網頁路徑才不會錯
+  base: '/Dietitian-workshop/', 
 })
